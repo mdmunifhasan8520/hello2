721942f509d72147b3ce0b631927d465cfcecc5d
pulok <mdmunifhasan8520@gmail.com>
2018-09-04 23:29:33 +0600

    Initial commit

---
+++ Tutorial_Learn_2_Build_apps.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Intro to variables and constants
+//what they are and why/how we use them
+//swift specific syntax
+
+//variable declaration
+//var/let name : type = value
+var name: String = "pulok"
+var number: Double = 2.0
+var health: Int = 100
+var floatNumber: Float = 1.0
+
+
+let maxHealth: Int = 150
+
+health = maxHealth
+health = 10
